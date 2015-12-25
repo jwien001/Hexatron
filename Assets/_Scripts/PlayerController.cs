46ef697884967e3a8691178d76356cbeffde8806
Josh Wien <jwien001@gmail.com>
2015-12-24 23:36:47 -0500

    Force player to always move forward

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -9,12 +9,7 @@ public class PlayerController : MonoBehaviour {
 	private static int RIGHT	= 60;
 
 	void Update() {
-		Vector3 movement = new Vector3();
-		if (Input.GetKey(KeyCode.W))
-			movement = transform.forward * speed * Time.deltaTime;
-		else if (Input.GetKey(KeyCode.S))
-			movement = -1 * transform.forward * speed * Time.deltaTime;
-		transform.position = movement + transform.position;
+		transform.position += transform.forward * speed * Time.deltaTime;
 
 		if (Input.GetKeyDown(KeyCode.A))
 			transform.Rotate(new Vector3(0, LEFT, 0));
